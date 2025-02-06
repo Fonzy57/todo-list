3f2105ee4fed97cad504ffa6f0f53f138db9d46c
Fonzy57 <traben@hotmail.fr>
2025-02-06 15:51:18 +0100

    Fix DatedTask toString method

--- src/task/DatedTask.java
+++ src/task/DatedTask.java
@@ -1,11 +1,15 @@
 package task;
 
+import user.User;
+
 import java.util.Date;
+
+import static java.lang.Math.round;
 
 public class DatedTask extends Task {
   private Date dueDate; // CHANGER LA DATE UTILISER LocalDate
 
-  public DatedTask(String title, String description, boolean done, double creator, Date dueDate) {
+  public DatedTask(String title, String description, boolean done, User creator, Date dueDate) {
     super(title, description, done, creator);
     this.dueDate = dueDate;
   }
@@ -16,5 +20,19 @@ public class DatedTask extends Task {
 
   public void setDueDate(Date dueDate) {
     this.dueDate = dueDate;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "ID :  %d %n" +
+            " DATED TASK %n" +
+            " Titre : %s %n" +
+            " Description : %s %n" +
+            " Tâche faite : %b %n" +
+            " Créateur de la tâche : %s" +
+            " Date de création : %s",
+        round(id), title, description, done, creator, dueDate
+    );
   }
 }
