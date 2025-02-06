7cfed11e67c7b4c53a0f05cb322573df43d0e54a
Fonzy57 <traben@hotmail.fr>
2025-02-06 16:01:42 +0100

    Add TaskBuilder class

--- src/task/TaskBuilder.java
+++ src/task/TaskBuilder.java
@@ -1,4 +1,34 @@
 package task;
 
+import user.User;
+
 public class TaskBuilder {
+  protected String title;
+  protected String description;
+  protected boolean done;
+  protected User creator;
+
+  public TaskBuilder setTitle(String title) {
+    this.title = title;
+    return this;
+  }
+
+  public TaskBuilder setDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public TaskBuilder setDone(boolean done) {
+    this.done = done;
+    return this;
+  }
+
+  public TaskBuilder setCreator(User creator) {
+    this.creator = creator;
+    return this;
+  }
+
+  public Task build() {
+    return new Task(title, description, done, creator);
+  }
 }
