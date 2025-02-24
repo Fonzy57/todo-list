12af4d9c281edd1131634754bb5df63c4bbafba5
Fonzy57 <traben@hotmail.fr>
2025-02-24 11:45:36 +0100

    Modify Database Access class with methods for H2 BDD

--- src/services/DatabaseAccess.java
+++ src/services/DatabaseAccess.java
@@ -5,13 +5,36 @@ import models.task.DatedTask;
 import models.task.Task;
 import models.user.User;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class DatabaseAccess {
-  private ArrayList<Task> tasks = new ArrayList<>();
-  private ArrayList<User> users = new ArrayList<>();
+  private static final String CREATE_TABLE_USERS = "CREATE TABLE IF NOT EXISTS USERS (id bigint auto_increment " +
+      "primary" +
+      " key, " +
+      "firstName" +
+      " " +
+      "varchar(255), lastName varchar(255));";
+  private static final String CREATE_TABLE_TASKS = "CREATE TABLE IF NOT EXISTS TASKS (id bigint auto_increment " +
+      "primary key, title " +
+      "varchar" +
+      "(255), description varchar(MAX), done boolean, creator_id bigint);";
+  private static final String CREATE_USER = "INSERT INTO USERS (firstName, lastName) VALUES (?, ?)";
+  private static final String CREATE_TASK = "INSERT INTO TASKS (title, description, done, creator_id) VALUES (?, ?, " +
+      "?," +
+      " " +
+      "?)";
+  private static final String GET_USER = "SELECT * FROM USERS WHERE id = ?";
+  private static final String GET_TASK = "SELECT * FROM TASKS WHERE id = ?";
+  private static final String UPDATE_USER = "UPDATE USERS SET firstName = ?, lastName = ? WHERE id = ?";
+  private static final String UPDATE_TASK = "UPDATE TASKS SET title = ?, description = ?, done= ? WHERE id = ?";
+  private static final String LIST_USERS = "SELECT * FROM USERS";
+  private static final String LIST_TASKS = "SELECT * FROM TASKS";
+  private static final String LIST_TASKS_DONE = "SELECT * FROM TASKS WHERE done = ?";
+
+  private Connection connection;
 
   // Instance
   private static DatabaseAccess instance;
@@ -26,102 +49,74 @@ public class DatabaseAccess {
 
   // Private constructor
   private DatabaseAccess() {
-    User steph = new User("Steph");
-    User hubert = new User("Hubert");
-
-    Task clean = new Task(
-        "Laver la maison",
-        "Laver la maison, c'est à dire passer l'aspirateur et la serpillères",
-        false,
-        steph
-    );
-
-    Task cleanCar = new Task(
-        "Laver la voiture",
-        "Laver la voiture en allant à l'éléphant bleu",
-        true,
-        steph
-    );
-
-    Task shopping = new Task(
-        "Faire les courses",
-        "Aller à Cora pour faire les courses. Voici la liste des aliments à acheter : \n  Pomme,\n  Dinde,\n  Pâtes," +
-            "\n  Riz,\n  Pesto,\n  Pepsi,\n  Monster",
-        true,
-        hubert
-    );
-
-    DatedTask giveProject = new DatedTask(
-        "Faire les courses",
-        "Aller à Cora Moulins-lès-Metz pour faire les courses.",
-        true,
-        hubert,
-        new Date()
-    );
-
-    // Adding tasks
-    tasks.add(clean);
-    tasks.add(cleanCar);
-    tasks.add(shopping);
-    tasks.add(giveProject);
-
-    // Adding users
-    users.add(steph);
-    users.add(hubert);
+    try {
+      connection = DriverManager.getConnection("jdbc:h2:mem:db1");
+      createTables();
+    } catch (SQLException e) {
+      System.err.println(e.getMessage());
+    }
   }
 
-  // -------------
-  // --- TASKS ---
-  // -------------
-
-  // LISTING TASKS
-  public List<Task> getAllTasks() {
-    return tasks;
-  }
-
-  // FIND TASK
-  public Task getTaskById(long id) throws ElementNotFoundException {
-    return tasks.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
-    // OU
-//    for (Task models.task : tasks) {
-//      if (models.task.getId() == id) {
-//        return models.task;
-//      }
-//    }
-//    throw new ElementNotFoundException("La tâche avec l'ID " + id + " n'existe pas");
-  }
-
-  // ADD TASK
-  public void addTask(Task task) {
-    tasks.add(task);
-  }
-
-  // DELETE TASK
-  public void deleteTaskById(long taskId) {
-    tasks.removeIf(t -> t.getId() == taskId);
+  /**
+   * Méthode pour créer les tables en BDD
+   */
+  private void createTables() throws SQLException {
+    connection.createStatement().executeUpdate(CREATE_TABLE_USERS);
+    connection.createStatement().executeUpdate(CREATE_TABLE_TASKS);
+    ResultSet rs = connection.createStatement().executeQuery("SHOW TABLES;");
+    while (rs.next()) {
+      System.out.println(rs.getString(1));
+    }
   }
 
   // -------------
   // --- USERS ---
   // -------------
 
-  // LISTING USERS
-  public List<User> getAllUsers() {
+  public void addUser(User user) {
+    try {
+      // Requête préparée
+      PreparedStatement statement = connection.prepareStatement(CREATE_USER);
+
+      // Équivalent au bind value
+      statement.setString(1, user.getFirstName());
+      statement.setString(2, user.getLastName());
+
+      // On exécute la requête
+      statement.executeUpdate();
+
+    } catch (SQLException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  public List<User> getUsers() {
+    List<User> users = new ArrayList<>();
+    try {
+      Statement stmt = connection.createStatement();
+      ResultSet result = stmt.executeQuery(GET_USER);
+
+      while (result.next()) {
+        Long id = result.getLong("id");
+        String firstName = result.getString("firstName");
+        String lastName = result.getString("lastName");
+        User user = new User(id, firstName, lastName);
+
+        users.add(user);
+      }
+
+      return users;
+
+    } catch (SQLException e) {
+      System.err.println(e.getMessage());
+    }
+
     return users;
   }
 
-  // FIND ONE USER BY ID
-  public User getUserById(long id) {
-    return users.stream().filter(u -> u.getId() == id).findFirst().orElse(null);
-  }
 
-  // ADD USER
-  public void addUser(User user) {
-    users.add(user);
-  }
+  // -------------
+  // --- TASKS ---
+  // -------------
 
-  // DELETE USER BY ID
-  public void deleteUserByID(long userID) {
-    users.removeIf(u -> u.getId() == userID);
-  }
 }
